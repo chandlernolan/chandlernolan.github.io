dbc902568937da050ddabca9398e8b2eaa3822b0
chandlernolan <chandlernolan@Chandlers-MacBook-Pro.local>
2016-11-29 23:50:31 -0800

    Push 11:50

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+
+$("#l3").click(function(){
+	$('html, body').animate(){
+    scrollTop: $("#contact-section").offset().top}, 500);
+
+});
+$("#l2").click(function(){
+	$('html, body').animate(){
+    scrollTop: $("#projects-section").offset().top}, 500);
+
+});
+$("#l1").click(function(){
+	$('html, body').animate(){
+    scrollTop: $("#contact-section").offset().top}, 500);
+
+});
+
+$("#l1").hover(function(){
+	$('html, body').animate(){
+    scrollTop: $("#contact-section").offset().top}, 500);
+
+});
+
+
+});
